87521a7147d442519dbc0c03770fc603b420692f
urjromero <ricardo.romero@ua.la>
2022-09-16 23:18:59 -0500

    Revert "example jsp and java beans using Jakarta EE 8 and deployed on Wildfly Server"
    This reverts commit 62f4a5ee0b7c26741e3b6e2c64539277b49d8ef3.

--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "downloading postgresql driver (42.5) ..."
-curl https://jdbc.postgresql.org/download/postgresql-42.5.0.jar -o /opt/resources/postgres.jar -s
-echo "executing data source script ..."
-/opt/jboss/wildfly/bin/jboss-cli.sh --file=/opt/resources/jndi.cli
-echo "data source added successfully :)"
-echo
